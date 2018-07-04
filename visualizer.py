ee3ae68886b27230939546c27856a368dc97f23f
Etienne Le Bouder <elebouder@gmail.com>
2018-07-03 20:00:05 -0700

    visualizer script to convert geohash aggregation csvs to ESRI GDAL .shp files for use with QGIS or ARCGIS

--- visualizer.py
+++ visualizer.py
@@ -1,59 +1,85 @@
 import gdal
 from gdal import osr, ogr
 
+rasterfile = ""
+csvfile = ""
+csvname = (csvfile.split('/')[-1]).split('.')[0]
 
 # use a dictionary reader so we can access by field name
-reader = csv.DictReader(open("volcano_data.txt","rb"),
-    delimiter='\t',
-    quoting=csv.QUOTE_NONE)
+
 
 # set up the shapefile driver
 driver = ogr.GetDriverByName("ESRI Shapefile")
 
 # create the data source
-data_source = driver.CreateDataSource("volcanoes.shp")
+data_source = driver.CreateDataSource(csvname + ".shp")
 
 # create the spatial reference, WGS84
 srs = osr.SpatialReference()
 srs.ImportFromEPSG(4326)
 
 # create the layer
-layer = data_source.CreateLayer("volcanoes", srs, ogr.wkbPoint)
+layer = data_source.CreateLayer(csvname + "_Aggregations", srs, ogr.wkbPoint)
 
 # Add the fields we're interested in
 field_name = ogr.FieldDefn("Name", ogr.OFTString)
 field_name.SetWidth(24)
 layer.CreateField(field_name)
-field_region = ogr.FieldDefn("Region", ogr.OFTString)
-field_region.SetWidth(24)
-layer.CreateField(field_region)
 layer.CreateField(ogr.FieldDefn("Latitude", ogr.OFTReal))
 layer.CreateField(ogr.FieldDefn("Longitude", ogr.OFTReal))
-layer.CreateField(ogr.FieldDefn("Elevation", ogr.OFTInteger))
 
-# Process the text file and add the attributes and features to the shapefile
-for row in reader:
-  # create the feature
-  feature = ogr.Feature(layer.GetLayerDefn())
-  # Set the attributes using the values from the delimited text file
-  feature.SetField("Name", row['Name'])
-  feature.SetField("Region", row['Region'])
-  feature.SetField("Latitude", row['Latitude'])
-  feature.SetField("Longitude", row['Longitude'])
-  feature.SetField("Elevation", row['Elev'])
+with open(csvfile, 'r') as csvf:
+    reader = csv.DictReader(csvf)
+    # Process the text file and add the attributes and features to the shapefile
+    for row in reader:
+        # create the feature
+        feature = ogr.Feature(layer.GetLayerDefn())
+        # Set the attributes using the values from the delimited text file
+        feature.SetField("Name", row['geohash'])
+        feature.SetField("Latitude", row['c_y'])
+        feature.SetField("Longitude", row['c_x'])
 
-  # create the WKT for the feature using Python string formatting
-  wkt = "POINT(%f %f)" %  (float(row['Longitude']) , float(row['Latitude']))
+  	# create the WKT for the feature using Python string formatting
+  	wkt = "POINT(%f %f)" %  (float(row['Longitude']) , float(row['Latitude']))
 
-  # Create the point from the Well Known Txt
-  point = ogr.CreateGeometryFromWkt(wkt)
+	# Create the point from the Well Known Txt
+  	point = ogr.CreateGeometryFromWkt(wkt)
 
-  # Set the feature geometry using the point
-  feature.SetGeometry(point)
-  # Create the feature in the layer (shapefile)
-  layer.CreateFeature(feature)
-  # Dereference the feature
-  feature = None
+  	# Set the feature geometry using the point
+  	feature.SetGeometry(point)
+  	# Create the feature in the layer (shapefile)
+  	layer.CreateFeature(feature)
+  	# Dereference the feature
+	feature = None
 
-# Save and close the data source
-data_source = None
+
+"""
+src = gdal.Open(infile)
+src_b = src.GetRasterBand(1)
+src_g = src.GetRasterBand(2)
+src_r = src.GetRasterBand(3)
+prj = src.GetProjection()
+
+switcher = {
+            'WGS 84 / UTM zone 8N': 32608,
+            'WGS 84 / UTM zone 9N': 32609,
+            'WGS 84 / UTM zone 10N': 32610,
+            'WGS 84 / UTM zone 11N': 32611,
+            'WGS 84 / UTM zone 12N': 32612
+        }
+
+srs = osr.SpatialReference(wkt=prj)
+if srs.IsProjected():
+	projcs = srs.GetAttrValue('projcs')
+        print projcs
+        epsg = switcher.get(projcs, 'nothing')
+
+if epsg == 'nothing':
+	print 'projection not in current list of projections handled by this code'
+        sys.exit(1)
+
+# epsg is 8901 for projected, 4326 for decimal lat/lon
+pproj = pyproj.Proj(init='epsg:%s' % epsg)
+praw = pyproj.Proj(init='epsg:4326')
+
+"""
